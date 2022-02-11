95374546c4c7c4c75d3dd838bcc54df43027a07f
pizdarya <85564410+pizdarya@users.noreply.github.com>
2022-02-11 13:36:47 +0300

    LIST

--- irc.hpp
+++ irc.hpp
@@ -13,7 +13,8 @@
 #include <csignal>
 #include "channel.hpp"
 #include "client.hpp"
-
+#include <iostream>
+#include <string>
 
 int sock;
 
@@ -724,11 +725,6 @@ public:
         closeAndErase(i);
     }
 
-    // void makeStringForNames(std::vector<Channel*>::iterator ch_it, int i, )
-    // {
-    //     std::string stringToSend = ":server 353 " + clients[i]->getNick() + " = " + (*ch_it)->getName() + " :";
-    // }
-
     void names(std::vector<std::string> cmd, int i)
     {
         std::string stringToSend;
@@ -761,6 +757,42 @@ public:
             ++ch_it;
         }
         stringToSend = ":server 366 " + clients[i]->getNick() + " * :End of /NAMES list\n";
+        send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+    }
+
+    void list(std::vector<std::string> cmd, int i)
+    {
+        std::string stringToSend = ":server 321 " + clients[i]->getNick() + " Channel :Users  Name\n";
+        send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+        stringToSend.clear();
+        std::vector<Channel*>::iterator ch_it = channels.begin();
+        std::vector<Channel*>::iterator ch_ite = channels.end();
+        if (cmd.size() > 1)
+        {
+            while (ch_it != ch_ite)
+            {
+                if ((*ch_it)->getName() == cmd[1])
+                {
+                    std::string stringToSend = ":server 322 " + clients[i]->getNick() + " " + (*ch_it)->getName() + 
+                            " " + std::to_string((*ch_it)->getClients()) + " :[+n] " + (*ch_it)->getTopic() + "\n";
+                    send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+                    stringToSend = ":server 323 " + clients[i]->getNick() + " :End of /LIST\n";
+                    send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+                }
+                ++ch_it;
+            }
+            stringToSend = ":server 403 " + clients[i]->getNick() + " " + cmd[1] + " :No such channel\n";
+            send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+            return;
+        }
+        while (ch_it != ch_ite)
+        {
+            std::string stringToSend = ":server 322 " + clients[i]->getNick() + " " + (*ch_it)->getName() + 
+                    " " + std::to_string((*ch_it)->getClients()) + " :[+n] " + (*ch_it)->getTopic() + "\n";
+            send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+            ++ch_it;
+        }
+        stringToSend = ":server 323 " + clients[i]->getNick() + " :End of /LIST\n";
         send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
     }
 
@@ -807,6 +839,8 @@ public:
                 quit(i);
             if (cmd[0] == "NAMES")
                 names(cmd, i);
+            if (cmd[0] == "LIST")
+                list(cmd, i);
         }
 
     }
