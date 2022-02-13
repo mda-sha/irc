b5da5784d39e43ba7f4a13063a7c2d6fecc63232
pizdarya <85564410+pizdarya@users.noreply.github.com>
2022-02-13 21:46:38 +0300

    wallops

--- irc.hpp
+++ irc.hpp
@@ -820,7 +820,12 @@ public:
             send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
             return;
         }
-        if (cmd.size() < 3)
+        if (cmd.size() < 2)
+        {
+            stringToSend = ":server 461 " + clients[i]->getNick() + "KILL :Not enough parameters\n";
+            send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+            return;
+        }
         if (!clients[i]->getOper())
         {
             stringToSend = ":server 461 " + clients[i]->getNick() + "KILL :Not enough parameters\n";
@@ -844,7 +849,28 @@ public:
         send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
     }
 
-
+    void wallops(std::vector<std::string> cmd, int i)
+    {
+        std::string stringToSend;
+        if (cmd.size() < 2)
+        {
+            stringToSend = ":server 461 " + clients[i]->getNick() + "WALLOPS :Not enough parameters\n";
+            send(clients[i]->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+            return;
+        }
+        stringToSend = ":" + clients[i]->getNick() + " WALLOPS :";
+        for (int i = 1; i < cmd.size(); i++)
+            stringToSend += cmd[i] + " ";
+        stringToSend += "\n";
+        std::vector<Client*>::iterator it = clients.begin();
+        std::vector<Client*>::iterator ite = clients.end();
+        while (it != ite)
+        {
+            if ((*it)->getOper())
+                send((*it)->clientSocket, stringToSend.c_str(), stringToSend.size(), 0);
+            ++it;
+        }
+    }
 
     void whichCmd(std::vector<std::string> cmd, int i)
     {
@@ -893,8 +919,8 @@ public:
                 list(cmd, i);
             if (cmd[0] == "KILL")
                 kill(cmd, i);
-            // if (cmd[0] == "WALLOPS")
-            //     wallops(cmd, i);
+            if (cmd[0] == "WALLOPS")
+                wallops(cmd, i);
 
         }
 
